7c73678afa80dd5f302d90655373959be612e034
HuhnCares <69119083+huhncares-cmd@users.noreply.github.com>
2023-01-22 17:14:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("menu");
+const nav = document.querySelector("nav");
+btn.addEventListener("click", () => {
+    toggleSidebar();
+});
+nav.addEventListener("click", () => {
+    toggleSidebar();
+});
+function toggleSidebar() {
+    document.querySelector("main").classList.toggle("active");
+    document.querySelector("header").classList.toggle("active");
+}
