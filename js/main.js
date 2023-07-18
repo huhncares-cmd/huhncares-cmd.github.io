f8f4e0c6df5a3ddcee521db91bcdf8d3b595b885
HuhnCares <huhncares@gmail.com>
2023-07-18 15:17:46 +0200

    Added sticky navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-//TODO: Nav-Sticky
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 0) {
+        document.querySelector("nav").classList.add("sticky", window.scrollY > 0);
+        return;
+    }
+    document.querySelector("nav").classList.remove("sticky", window.scrollY == 0);
+});
