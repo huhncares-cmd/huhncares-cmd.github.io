6f0549b01f1b6322709f6be90f587917adeef13f
Raphael Matjeschk <huhncares@gmail.com>
2024-05-08 15:51:56 +0200

    Entfernte nicht verwendeten Code in js/main.js, themen/main.js, css/gh-cards.css, js/github-cards.js, css/style.css, assets/icons/github.svg und assets/icons/twitter.svg

--- js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-    if(window.scrollY > 0) {
-        document.querySelector("nav").classList.add("sticky", window.scrollY > 0);
-        return;
-    }
-    document.querySelector("nav").classList.remove("sticky", window.scrollY == 0);
-});
